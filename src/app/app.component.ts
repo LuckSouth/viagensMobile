4fe7ef40fad515a29c7e07e8cbc036f471e75393
Henrique Moreira <henriquemoreira842@gmail.com>
2018-01-29 10:48:50 -0300

    v 1.4.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,14 +18,14 @@ import { GooglePlus } from '@ionic-native/google-plus';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  
-  rootPage: any = LoginPage;
+
+  rootPage: any;
   storages: any;
   listaAuth;
-  chaveAuth: string = "Auth"; 
+  chaveAuth: string = "Auth";
   listaDespesas = {};
-  
-  
+
+
   constructor(
     platform: Platform,
     statusBar: StatusBar,
@@ -37,32 +37,17 @@ export class MyApp {
     // public navCtrl: NavController
   ) {
 
-    this.googlePlus.trySilentLogin({}).then(
-      () => this.rootPage = PrincipalPage
-    ).catch(err => console.error(err));
-    
-    
+
     platform.ready().then(() => {
-      
-      // this.storage.ready().then(() => {
-      //   this.storage.get("Auth").then((registros) => {
-      //     this.listaAuth = registros;
-      //     console.log(this.listaAuth.isLoggedIn);
-      //     if(this.listaAuth.isLoggedIn){
-      //       this.rootPage = PrincipalPage;
-      //     }
-      //     if(this.listaAuth.isLoggedIn == undefined){
-      //       this.rootPage = LoginPage;
-      //     }
-         
-      //   })
-      // })
-
-
       this.screenOrientation.lock(screenOrientation.ORIENTATIONS.PORTRAIT);
       statusBar.styleDefault();
       splashScreen.hide();
+
+      
+      this.googlePlus.trySilentLogin({}).then(
+        () => this.rootPage = PrincipalPage
+      ).catch(res => this.rootPage = LoginPage);
     });
-    
+
   }
 }
