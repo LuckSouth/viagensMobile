291159de76d0b052eb7771fe53a15a7594221f01
Arthursm <yekuunn@gmail.com>
2018-04-14 09:20:12 -0300

    v 3.0.4

--- src/pages/modulo-vendas/produtos/produtos.ts
+++ src/pages/modulo-vendas/produtos/produtos.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ProdutosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +8,54 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'produtos.html',
 })
 export class ProdutosPage {
-
+  items: any[];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.initializeItems()
+  }
+  
+  initializeItems() {
+    this.items = [{
+      "nome": "A22 - Frango Congelado 1.7 Encaixado",
+      "preco": "R$ 5,00",
+      "qtd": "200 KG",
+      "id_produto": "1"
+    },
+    {
+      "nome": "A22 - Ovo industrial 6 ",
+      "preco": "R$ 6,60",
+      "qtd": "2500 bd",
+      "id_produto": "2"
+    }]
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProdutosPage');
+  e(item) {
+    console.log(item)
+
   }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+    
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((items) => {
+        if (items.nome.toLowerCase().indexOf(val.toLowerCase()) > -1) {
+          return (items.nome.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        }
+        if (items.preco.toLowerCase().indexOf(val.toLowerCase()) > -1) {
+          return (items.preco.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        }
+        if (items.qtd.toLowerCase().indexOf(val.toLowerCase()) > -1) {
+          return (items.qtd.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        }
+        
+      })
+
+    }
+  }
+
 
 }
